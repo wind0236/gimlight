0fe4a327e0adb5d1ae5f29dd0260aa561342bd21
Hiroki Tokunaga <tokusan441@gmail.com>
2021-09-19 16:39:27 +0900

    chore: move `@`

--- app/Game.hs
+++ app/Game.hs
@@ -3,20 +3,25 @@
 
 module Game where
 
-import           Control.Lens (makeLenses)
-import           Linear.V2    (V2 (..))
+import           Control.Lens                   (makeLenses, (%~), (&), (.=),
+                                                 (.~))
+import           Control.Monad.Trans.Maybe      (MaybeT (MaybeT), runMaybeT)
+import           Control.Monad.Trans.State.Lazy (execState, modify)
+import           Linear.V2                      (V2 (..), _x, _y)
 
 initGame :: IO Game
 initGame = do
+        let xm = width `div` 2
+        let ym = height `div` 2
         let g = Game {
-                     _player = V2 0 0
+                     _player = V2 xm ym
                      }
         return g
 
 
 height, width :: Int
-height = 80
-width = 50
+height = 30
+width = 80
 
 type Coord = V2 Int
 
@@ -25,3 +30,17 @@ data Game = Game
           } deriving (Show)
 
 makeLenses ''Game
+
+move :: Direction -> Game -> Game
+move d g = flip execState g . runMaybeT $ do
+    MaybeT . fmap Just $ player .= nextPosition d g
+
+nextPosition :: Direction -> Game -> Coord
+nextPosition d Game { _player = p }
+    | d == North = p & _y %~ (\y -> min (y + 1) (height - 1))
+    | d == South = p & _y %~ (\y -> max (y - 1) 0)
+    | d == East  = p & _x %~ (\x -> min (x + 1) (width - 1))
+    | d == West  = p & _x %~ (\x -> max (x - 1) 0)
+nextPosition _ _ = error "unreachable"
+
+data Direction = North | South | East | West deriving (Eq, Show)
--- app/UI.hs
+++ app/UI.hs
@@ -3,19 +3,20 @@
 module UI(main) where
 
 import           Brick                      (App (..), AttrMap, AttrName,
-                                             BrickEvent (VtyEvent), EventM,
-                                             Next, Widget, attrMap, continue,
-                                             customMain, hBox, halt,
+                                             BrickEvent (AppEvent, VtyEvent),
+                                             EventM, Next, Widget, attrMap,
+                                             continue, customMain, hBox, halt,
                                              neverShowCursor, on, str, vBox,
                                              withBorderStyle)
 import           Brick.BChan                (newBChan, writeBChan)
+import qualified Brick.Widgets.Border       as B
 import qualified Brick.Widgets.Border.Style as BS
 import qualified Brick.Widgets.Center       as C
 import           Control.Concurrent         (forkIO, threadDelay)
 import           Control.Lens               ((^.))
 import           Control.Monad              (forever, void)
-import           Game                       (Game, height, initGame, player,
-                                             width)
+import           Game                       (Direction (..), Game, height,
+                                             initGame, move, player, width)
 import qualified Graphics.Vty               as V
 import           Linear.V2                  (V2 (..))
 
@@ -43,6 +44,14 @@ app = App { appDraw = drawUI
 handleEvent :: Game -> BrickEvent Name Tick -> EventM Name (Next Game)
 handleEvent g (VtyEvent (V.EvKey (V.KChar 'q') [])) = halt g
 handleEvent g (VtyEvent (V.EvKey V.KEsc []))        = halt g
+handleEvent g (VtyEvent (V.EvKey V.KUp []))         = continue $ move North g
+handleEvent g (VtyEvent (V.EvKey V.KDown []))       = continue $ move South g
+handleEvent g (VtyEvent (V.EvKey V.KRight []))      = continue $ move East g
+handleEvent g (VtyEvent (V.EvKey V.KLeft []))       = continue $ move West g
+handleEvent g (VtyEvent (V.EvKey (V.KChar 'k') [])) = continue $ move North g
+handleEvent g (VtyEvent (V.EvKey (V.KChar 'j') [])) = continue $ move South g
+handleEvent g (VtyEvent (V.EvKey (V.KChar 'l') [])) = continue $ move East g
+handleEvent g (VtyEvent (V.EvKey (V.KChar 'h') [])) = continue $ move West g
 handleEvent g _                                     = continue g
 
 type Name = ()
@@ -51,14 +60,16 @@ drawUI :: Game -> [Widget Name]
 drawUI g = [ C.center $ drawGame g ]
 
 drawGame :: Game -> Widget Name
-drawGame g = withBorderStyle BS.unicodeBold $ vBox rows
+drawGame g = withBorderStyle BS.unicodeBold
+    $ B.borderWithLabel (str "Game")
+    $ vBox rows
     where
         rows = [hBox $ cellsInRow r | r <- [height - 1, height -2 .. 0]]
         cellsInRow y = [putCoord (V2 x y)  | x <- [ 0 .. width - 1]]
         putCoord = str . cellAt
         cellAt c = if g ^. player == c
                     then "@"
-                    else ""
+                    else " "
 
 theMap :: AttrMap
 theMap = attrMap V.defAttr
