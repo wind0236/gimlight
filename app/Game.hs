79d8980c4d0b8509a568925200b48ea566f3fdf5
Hiroki Tokunaga <tokusan441@gmail.com>
2021-09-20 11:00:57 +0900

    chore: color support

--- app/Game.hs
+++ app/Game.hs
@@ -9,46 +9,39 @@ import           Control.Lens                   (makeLenses, (%~), (&), (.=),
                                                  (.~))
 import           Control.Monad.Trans.Maybe      (MaybeT (MaybeT), runMaybeT)
 import           Control.Monad.Trans.State.Lazy (execState, modify)
+import           Data.Array                     (Array)
+import           Data.Array.Base                (array, (//))
 import           Graphics.Vty.Attributes.Color  (Color, white, yellow)
 import           Linear.V2                      (V2 (..), _x, _y)
-
-initGame :: IO Game
-initGame = do
-        let xm = width `div` 2
-        let ym = height `div` 2
-        let player = Entity { _position = V2 xm ym
-                            , _char = "@"
-                            , _attr = "playerAttr"
-                            }
-        let npc = Entity { _position = V2 (xm - 5) ym
-                         , _char = "@"
-                         , _attr = "npcAttr"
-                         }
-        let g = Game { _player = player
-                     , _npc = npc
-                     }
-        return g
-
 
 height, width :: Int
 height = 30
 width = 80
 
 type Coord = V2 Int
+type Map = Array (Int, Int) Tile
 
 data Game = Game
-          { _player :: Entity
-          , _npc    :: Entity
+          { _player  :: Entity
+          , _npc     :: Entity
+          , _gameMap :: Map
           } deriving (Show)
 
 data Entity = Entity
-            { _position :: Coord
-            , _char     :: [Char]
-            , _attr     :: AttrName
+            { _position   :: Coord
+            , _char       :: [Char]
+            , _entityAttr :: AttrName
             } deriving (Show)
+
+data Tile = Tile
+          { _walkable    :: Bool
+          , _transparent :: Bool
+          , _tileAttr    :: AttrName
+          } deriving (Show)
 
 makeLenses ''Game
 makeLenses ''Entity
+makeLenses ''Tile
 
 move :: Direction -> Game -> Game
 move d g = flip execState g . runMaybeT $ do
@@ -69,4 +62,41 @@ nextPosition _ _ = error "unreachable"
 entities :: Game -> [Entity]
 entities Game { _player = player, _npc = npc } = [player, npc]
 
+initMap :: Array (Int, Int) Tile
+initMap = emptyTiles // [((x, 22), wallTile) | x <- [30 .. 32]]
+
+emptyTiles :: Array (Int, Int) Tile
+emptyTiles = array ((0, 0), (width - 1, height - 1))
+    [((x, y), floorTile) | x <- [0 .. width - 1], y <- [0 .. height - 1]]
+
+wallTile :: Tile
+wallTile = Tile { _walkable = False
+            , _transparent = False
+            , _tileAttr = "wallAttr"
+            }
+
+floorTile :: Tile
+floorTile = Tile { _walkable = True
+             , _transparent = True
+             , _tileAttr = "floorAttr"
+             }
+
 data Direction = North | South | East | West deriving (Eq, Show)
+
+initGame :: IO Game
+initGame = do
+        let xm = width `div` 2
+        let ym = height `div` 2
+        let player = Entity { _position = V2 xm ym
+                            , _char = "@"
+                            , _entityAttr = "playerAttr"
+                            }
+        let npc = Entity { _position = V2 (xm - 5) ym
+                         , _char = "@"
+                         , _entityAttr = "npcAttr"
+                         }
+        let g = Game { _player = player
+                     , _npc = npc
+                     , _gameMap = initMap
+                     }
+        return g
--- app/UI.hs
+++ app/UI.hs
@@ -15,11 +15,13 @@ import qualified Brick.Widgets.Center       as C
 import           Control.Concurrent         (forkIO, threadDelay)
 import           Control.Lens               ((&), (^.))
 import           Control.Monad              (forever, void)
-import           Game                       (Direction (..), Game, attr, char,
-                                             entities, height, initGame, move,
-                                             player, position, width)
+import           Data.Array.Base            ((!))
+import           Game                       (Direction (..), Game, char,
+                                             entities, entityAttr, gameMap,
+                                             height, initGame, move, player,
+                                             position, tileAttr, width)
 import qualified Graphics.Vty               as V
-import           Linear.V2                  (V2 (..))
+import           Linear.V2                  (V2 (..), _x, _y)
 
 main :: IO ()
 main = do
@@ -67,19 +69,25 @@ drawGame g = withBorderStyle BS.unicodeBold
     where
         rows = [hBox $ cellsInRow r | r <- [height - 1, height - 2 .. 0]]
         cellsInRow y = [cellAt (V2 x y)  | x <- [0 .. width - 1]]
+        tileOnCellAt c = (g ^. gameMap) ! (c ^. _x, c ^. _y)
         entityOnCellAt c = [e | e <- entities g, e ^. position == c]
         cellAt c = let entityAt = entityOnCellAt c
                        in case entityAt
                        of
-                        entity:_ -> withAttr (entity ^. attr) $ str $ entity ^. char
-                        []       -> withAttr emptyAttr $ str " "
+                        entity:_ -> withAttr (entity ^. entityAttr) $ str $ entity ^. char
+                        []       -> withAttr (tileOnCellAt c ^. tileAttr) $ str " "
 
 theMap :: AttrMap
 theMap = attrMap V.defAttr
-    [(playerAttr, V.brightWhite `on` V.black),
-     (npcAttr, V.yellow `on` V.black)]
+    [ (playerAttr, fg V.brightWhite)
+    , (npcAttr, fg V.yellow)
+    , (floorAttr, V.rgbColor 255 255 255 `on` V.rgbColor 50 50 150)
+    , (wallAttr, V.rgbColor 255 255 255 `on` V.rgbColor 0 0 100)
+    ]
 
-playerAttr, npcAttr, emptyAttr :: AttrName
+playerAttr, npcAttr, emptyAttr, floorAttr, wallAttr :: AttrName
 playerAttr = "playerAttr"
 npcAttr = "npcAttr"
 emptyAttr = "emptyAttr"
+floorAttr = "floorAttr"
+wallAttr = "wallAttr"
