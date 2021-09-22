44f1ddb584cdf66b68f42f500ac056b00b834387
Hiroki Tokunaga <tokusan441@gmail.com>
2021-09-22 17:59:48 +0900

    chore: create `Generate.hs`

--- app/Game.hs
+++ app/Game.hs
@@ -14,9 +14,9 @@ import           Data.Array                     (Array)
 import           Data.Array.Base                (array, bounds, elems, (!),
                                                  (//))
 import           Direction                      (Direction (East, North, South, West))
-import           Dungeon                        (initDungeon)
 import           Dungeon.BoolMap                (BoolMap, emptyBoolMap)
 import           Dungeon.GameMap                (GameMap)
+import           Dungeon.Generate               (initDungeon)
 import           Dungeon.Size                   (height, width)
 import           Dungeon.Tile                   (Tile, darkAttr, lightAttr,
                                                  transparent, walkable)
