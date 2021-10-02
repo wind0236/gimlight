d892fe8d3d1a97c4eba8ead4c20eb26b44fc02e7
Hiroki Tokunaga <tokusan441@gmail.com>
2021-10-02 11:29:18 +0900

    style: add a blank line (#28)

--- app/Entity.hs
+++ app/Entity.hs
@@ -112,6 +112,7 @@ updateHp e@Actor{ _hp = hp, _maxHp = maxHp } newHp =
         in if hpInRange == 0 && e ^. isAlive
                then  die e
                else e{ _hp = max 0 $ min maxHp newHp }
+
 die :: Entity -> Entity
 die e = e{ _hp = 0
          , _char = "%"
