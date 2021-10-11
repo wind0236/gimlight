a575ba1a56e604592f88b31663ea2e6bf91d6083
Hiroki Tokunaga <tokusan441@gmail.com>
2021-10-11 13:19:14 +0900

    refactor: use `OverloadedStrings` (#81)

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import           Data.Text (pack)
-import           Engine    (initEngine)
-import           Monomer   (MainWindowState (MainWindowNormal), appFontDef,
-                            appInitEvent, appTheme, appWindowResizable,
-                            appWindowState, appWindowTitle, darkTheme, startApp)
-import           UI.Draw   (drawUI, windowHeight, windowWidth)
-import qualified UI.Event  as E
-import           UI.Types  (AppEvent (..))
+import           Engine   (initEngine)
+import           Monomer  (MainWindowState (MainWindowNormal), appFontDef,
+                           appInitEvent, appTheme, appWindowResizable,
+                           appWindowState, appWindowTitle, darkTheme, startApp)
+import           UI.Draw  (drawUI, windowHeight, windowWidth)
+import qualified UI.Event as E
+import           UI.Types (AppEvent (..))
 
 main :: IO ()
 main = startApp model handleEvent buildUI config
@@ -15,9 +16,9 @@ main = startApp model handleEvent buildUI config
         model = initEngine
         handleEvent = E.handleEvent
         buildUI = drawUI
-        config = [ appWindowTitle (pack "Roguelike")
+        config = [ appWindowTitle "Roguelike"
                  , appTheme darkTheme
-                 , appFontDef (pack "Regular") (pack "third_party/noto-cjk/NotoSansCJK-VF.otf.ttc")
+                 , appFontDef "Regular" "third_party/noto-cjk/NotoSansCJK-VF.otf.ttc"
                  , appInitEvent AppInit
                  , appWindowState $ MainWindowNormal (windowWidth, windowHeight)
                  , appWindowResizable False
