cdda63cb27fe9233b80086246f2992251c0a17a8
Hiroki Tokunaga <tokusan441@gmail.com>
2021-10-14 15:21:59 +0900

    style: fix indentations (#99)

--- src/Scene.hs
+++ src/Scene.hs
@@ -21,9 +21,9 @@ newtype SceneElement = WithoutSpeaker Text deriving (Show, Eq, Ord, Generic)
 instance Binary SceneElement
 
 data Scene = Scene
-           { _backgroundImage :: Text
-           , _elements        :: [SceneElement]
-           } deriving (Show, Ord, Eq, Generic)
+    { _backgroundImage :: Text
+    , _elements        :: [SceneElement]
+    } deriving (Show, Ord, Eq, Generic)
 makeLenses ''Scene
 instance Binary Scene
 
@@ -34,9 +34,10 @@ text :: SceneElement -> Text
 text (WithoutSpeaker t) = t
 
 gameStartScene :: Scene
-gameStartScene = Scene { _backgroundImage = "images/game_opening.png"
-                       , _elements = xs
-                       }
+gameStartScene = Scene
+    { _backgroundImage = "images/game_opening.png"
+    , _elements = xs
+    }
     where xs = [ withoutSpeaker "Planet Kantsarta is a strange planet. There lives not only human, animals, and plants, but also therianthropies, fairies, undeads, and even dragons."
                , withoutSpeaker "And you will leave from Beaeve, a country town located in one of countries existing on the planet, Karpho. Without knowing what will happen in the future. Having a great hope."
                ]
