c36dab92fdbe0eb47fad51558ba91069c48aaace
Hiroki Tokunaga <tokusan441@gmail.com>
2021-10-15 20:15:10 +0900

    refactor: remove unnecessary functions (#108)

--- src/Log.hs
+++ src/Log.hs
@@ -7,36 +7,23 @@ module Log
     , message
     , addMessage
     , addMaybeMessage
-    , messageToTextList
-    , messageToAttrNameAndTextList
-    , height
-    , width
-    , emptyMessage
     , addMessages
     ) where
 
-import           Data.Text (Text, append, pack, splitOn)
-import qualified Data.Text as T
+import           Data.Text (Text)
 
 type Message = Text
 
 type MessageLog = [Message]
 
-width, height :: Int
-width = 80
-height = 5
-
 emptyLog :: MessageLog
-emptyLog = take height $ replicate height emptyMessage
-
-emptyMessage :: Message
-emptyMessage = pack $ replicate width ' '
+emptyLog = []
 
 addMessages :: [Message] -> MessageLog -> MessageLog
 addMessages xs l = foldl (flip addMessage) l xs
 
 addMessage :: Message -> MessageLog -> MessageLog
-addMessage m l = m:l
+addMessage m l = take maxLog (m:l)
 
 addMaybeMessage :: Maybe Message -> MessageLog -> MessageLog
 addMaybeMessage (Just m) l = addMessage m l
@@ -45,20 +32,5 @@ addMaybeMessage Nothing l  = l
 message :: Text -> Message
 message text = text
 
-messageToAttrNameAndTextList :: Message -> [Text]
-messageToAttrNameAndTextList m =  take height $ messageToTextList m
-
-messageToTextList :: Message -> [Text]
-messageToTextList text = map (\x -> x `append` pack (replicate (width - T.length x) ' ')) $ concatMap wrapText $ splitTextOnNewLine text
-
-wrapText :: Text -> [Text]
-wrapText = wrapTextAcc []
-
-wrapTextAcc :: [Text] -> Text -> [Text]
-wrapTextAcc s "" = s
-wrapTextAcc list str = wrapTextAcc newList newStr
-    where (newLine, newStr) = T.splitAt width str
-          newList = list ++ [newLine]
-
-splitTextOnNewLine :: Text -> [Text]
-splitTextOnNewLine = splitOn "\n"
+maxLog :: Int
+maxLog = 100
