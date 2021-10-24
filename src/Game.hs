7241b6f8596413103f794834fd21724dfe98ae80
Hiroki Tokunaga <tokusan441@gmail.com>
2021-10-24 09:13:32 +0900

    docs: add a note (#152)

--- src/Game.hs
+++ src/Game.hs
@@ -1,3 +1,7 @@
+-- You should not define any functions in this module and instead just
+-- expose `Game`'s internal structure. Otherwise, we will drown in lots of
+-- functions.
+
 module Game
     ( Game(..)
     ) where
