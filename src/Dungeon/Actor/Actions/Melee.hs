2420c8899b3d65589346b129cbf6748518370ce3
Hiroki Tokunaga <tokusan441@gmail.com>
2021-10-27 14:43:55 +0900

    refactor: split into functions (#161)

--- src/Dungeon/Actor/Actions/Melee.hs
+++ src/Dungeon/Actor/Actions/Melee.hs
@@ -6,13 +6,13 @@ module Dungeon.Actor.Actions.Melee
 
 import           Control.Lens          ((^.))
 import           Data.Text             (append, pack)
-import           Dungeon               (popActorAt, pushActor)
-import           Dungeon.Actor         (defence, getHp, name, position, power,
-                                        updateHp)
+import           Dungeon               (Dungeon, popActorAt, pushActor)
+import           Dungeon.Actor         (Actor, defence, getHp, name, position,
+                                        power, updateHp)
 import           Dungeon.Actor.Actions (Action)
 import           Linear.V2             (V2)
 import           Localization          (multilingualText)
-import           Log                   (message)
+import           Log                   (Message, MessageLog, message)
 
 meleeAction :: V2 Int -> Action
 meleeAction offset src dungeon =
@@ -23,36 +23,43 @@ meleeAction offset src dungeon =
           result =
             case target of
                 Nothing       -> (([], False), pushActor src dungeon)
-                Just defender -> attackFromTo src defender
-
-          attackFromTo attacker defender =
-            let damage = attacker ^. power - defender ^. defence
-            in if damage > 0
-                  then let newHp = getHp defender - damage
-                           newDefender = updateHp defender newHp
-                           messages = if newHp <= 0
-                                          then [damagedMessage attacker defender damage, deathMessage defender]
-                                          else [damagedMessage attacker defender damage]
-                           actorHandler = if newHp > 0
-                                            then pushActor attacker . pushActor newDefender
-                                            else pushActor attacker
-                           dungeonAfterAttack = actorHandler dungeonWithoutTarget
-                       in ((map message messages, True), dungeonAfterAttack)
-                    else (([message $ noDamage attacker defender], True), pushActor attacker $ pushActor defender dungeonWithoutTarget)
+                Just defender -> attackFromTo src defender dungeonWithoutTarget
 
 
-          attackMessage from to =
-            mconcat [ from ^. name
-                    , multilingualText " attacks" "は"
-                    , to ^. name
-                    , multilingualText "" "に攻撃"
-                    ]
+attackFromTo :: Actor -> Actor -> Dungeon -> ((MessageLog, Bool), Dungeon)
+attackFromTo attacker defender dungeonWithoutTarget =
+  let damage = attacker ^. power - defender ^. defence
+  in if damage > 0
+        then let newHp = getHp defender - damage
+                 newDefender = updateHp defender newHp
+                 messages = if newHp <= 0
+                                then [damagedMessage attacker defender damage, deathMessage defender]
+                                else [damagedMessage attacker defender damage]
+                 actorHandler = if newHp > 0
+                                  then pushActor attacker . pushActor newDefender
+                                  else pushActor attacker
+                 dungeonAfterAttack = actorHandler dungeonWithoutTarget
+             in ((map message messages, True), dungeonAfterAttack)
+          else (([message $ noDamageMessage attacker defender], True), pushActor attacker $ pushActor defender dungeonWithoutTarget)
 
-          damagedMessage from to damage =
+damagedMessage :: Actor -> Actor -> Int -> Message
+damagedMessage from to damage =
+    attackMessage from to <>
+        multilingualText (" for " `append` pack (show damage) `append` " hit points.")
+                         ("して" `append` pack (show damage) `append` "ポイントのダメージを与えた．")
 
-              attackMessage from to <>
-                multilingualText (" for " `append` pack (show damage) `append` " hit points.")
-                                 ("して" `append` pack (show damage) `append` "ポイントのダメージを与えた．")
-          deathMessage who = (who ^. name) <> multilingualText " is dead!" "は死んだ．"
-          noDamage from to = attackMessage from to `mappend`
-                                multilingualText " but does not damage." "したがダメージを受けなかった．"
+deathMessage :: Actor -> Message
+deathMessage who = (who ^. name) <> multilingualText " is dead!" "は死んだ．"
+
+attackMessage :: Actor -> Actor -> Message
+attackMessage from to =
+    mconcat [ from ^. name
+            , multilingualText " attacks" "は"
+            , to ^. name
+            , multilingualText "" "に攻撃"
+            ]
+
+
+noDamageMessage :: Actor -> Actor -> Message
+noDamageMessage from to = attackMessage from to `mappend`
+                    multilingualText " but does not damage." "したがダメージを受けなかった．"
