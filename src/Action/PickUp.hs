87afb8b941028543145e56683f60d0ecb11dbf9e
Hiroki Tokunaga <tokusan441@gmail.com>
2021-11-19 18:22:08 +0900

    refactor: split `pickUpAction` (#251)

--- src/Action/PickUp.hs
+++ src/Action/PickUp.hs
@@ -3,34 +3,37 @@ module Action.PickUp
     ) where
 
 import           Action               (Action, ActionResult (ActionResult),
+                                       ActionResultWithLog,
                                        ActionStatus (Failed, Ok))
 import           Actor                (inventoryItems, position)
-import           Actor.Inventory      (addItem)
+import           Actor.Inventory      (Inventory, addItem)
 import           Control.Lens         ((&), (.~), (^.))
 import           Control.Monad.Writer (tell)
 import           Dungeon              (popItemAt, pushActor)
-import           Item                 (getName)
+import           Item                 (Item, getName)
 import qualified Localization.Texts   as T
 
 pickUpAction :: Action
-pickUpAction e d =
-    case item of
-        Just x ->
-            case addItem x (e ^. inventoryItems) of
-                Just xs -> do
-                    tell [T.youGotItem $ getName x]
-                    return $
-                        ActionResult
-                            Ok
-                            (pushActor
-                                 (e & inventoryItems .~ xs)
-                                 dungeonAfterPickingUp)
-                            []
-                Nothing -> do
-                    tell [T.bagIsFull]
-                    return $ ActionResult Failed (pushActor e d) []
-        Nothing -> do
-            tell [T.youGotNothing]
-            return $ ActionResult Failed (pushActor e d) []
+pickUpAction e d = maybe youGotNothing pickUpItem item
   where
+    pickUpItem i =
+        case addItem i (e ^. inventoryItems) of
+            Just xs -> pickUpSucceed i xs
+            Nothing -> bagIsFull
+    pickUpSucceed :: Item -> Inventory -> ActionResultWithLog
+    pickUpSucceed i newInventory = do
+        tell [T.youGotItem $ getName i]
+        return $
+            ActionResult
+                Ok
+                (pushActor
+                     (e & inventoryItems .~ newInventory)
+                     dungeonAfterPickingUp)
+                []
+    bagIsFull = do
+        tell [T.bagIsFull]
+        return $ ActionResult Failed (pushActor e d) []
+    youGotNothing = do
+        tell [T.youGotNothing]
+        return $ ActionResult Failed (pushActor e d) []
     (item, dungeonAfterPickingUp) = popItemAt (e ^. position) d
