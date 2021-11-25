1b971665bc1a3f5a606728b49154fc1bc7ea37d2
Hiroki Tokunaga <tokusan441@gmail.com>
2021-11-25 21:50:28 +0900

    refactor: rewrite with `where` (#265)

--- src/Player.hs
+++ src/Player.hs
@@ -37,21 +37,23 @@ import           Linear.V2                (V2)
 
 handlePlayerMoving :: V2 Int -> ExploringHandler -> GameStatus
 handlePlayerMoving offset gs =
-    let (isSuccess, newState) = playerBumpAction offset gs
-     in if isSuccess
-            then case newState of
-                     Exploring eh ->
-                         maybe GameOver Exploring (processAfterPlayerTurn eh)
-                     _ -> newState
-            else newState
+    if isSuccess
+        then case newState of
+                 Exploring eh ->
+                     maybe GameOver Exploring (processAfterPlayerTurn eh)
+                 _ -> newState
+        else newState
+  where
+    (isSuccess, newState) = playerBumpAction offset gs
 
 handlePlayerPickingUp :: ExploringHandler -> GameStatus
 handlePlayerPickingUp eh =
-    let (status, newHandler) = doPlayerAction pickUpAction eh
-     in case status of
-            Ok -> maybe GameOver Exploring $ processAfterPlayerTurn newHandler
-            ReadingStarted _ -> error "Unreachable."
-            Failed -> Exploring newHandler
+    case status of
+        Ok -> maybe GameOver Exploring $ processAfterPlayerTurn newHandler
+        ReadingStarted _ -> error "Unreachable."
+        Failed -> Exploring newHandler
+  where
+    (status, newHandler) = doPlayerAction pickUpAction eh
 
 handlePlayerSelectingItem :: Reason -> ExploringHandler -> GameStatus
 handlePlayerSelectingItem reason eh =
@@ -82,13 +84,12 @@ handlePlayerAfterSelecting h = result
             Drop -> dropAction
 
 playerBumpAction :: V2 Int -> ExploringHandler -> (Bool, GameStatus)
-playerBumpAction offset eh =
-    let action =
-            case actorAtDestination of
-                Just x  -> meleeOrTalk offset x
-                Nothing -> moveOrExitMap offset
-     in action eh
+playerBumpAction offset eh = action eh
   where
+    action =
+        case actorAtDestination of
+            Just x  -> meleeOrTalk offset x
+            Nothing -> moveOrExitMap offset
     actorAtDestination =
         find (\x -> x ^. position == destination) $
         getActors $ getCurrentDungeon eh
@@ -100,31 +101,31 @@ playerBumpAction offset eh =
 meleeOrTalk :: V2 Int -> Actor -> ExploringHandler -> (Bool, GameStatus)
 meleeOrTalk offset target eh
     | isMonster target =
-        let (status, newHandler) = doPlayerAction (meleeAction offset) eh
-         in case status of
-                Ok               -> (True, Exploring newHandler)
-                ReadingStarted _ -> error "Unreachable."
-                Failed           -> (False, Exploring newHandler)
+        case status of
+            Ok               -> (True, Exploring newHandler)
+            ReadingStarted _ -> error "Unreachable."
+            Failed           -> (False, Exploring newHandler)
     | otherwise =
         case getTalkingPart target of
             Just x  -> (True, Talking $ talkingHandler target x eh)
             Nothing -> error "No talk handler is set."
+  where
+    (status, newHandler) = doPlayerAction (meleeAction offset) eh
 
 moveOrExitMap :: V2 Int -> ExploringHandler -> (Bool, GameStatus)
 moveOrExitMap offset eh =
-    let destination =
-            case getPlayerPosition eh of
-                Just p  -> p + offset
-                Nothing -> error "The player is dead."
-     in if isPositionInDungeon destination eh ||
-           not (isTown (getCurrentDungeon eh))
-            then let (status, newHandler) =
-                         doPlayerAction (moveAction offset) eh
-                  in case status of
-                         Ok               -> (True, Exploring newHandler)
-                         ReadingStarted _ -> error "Unreachable."
-                         Failed           -> (False, Exploring newHandler)
-            else (True, exitDungeon eh)
+    if isPositionInDungeon destination eh || not (isTown (getCurrentDungeon eh))
+        then let (status, newHandler) = doPlayerAction (moveAction offset) eh
+              in case status of
+                     Ok               -> (True, Exploring newHandler)
+                     ReadingStarted _ -> error "Unreachable."
+                     Failed           -> (False, Exploring newHandler)
+        else (True, exitDungeon eh)
+  where
+    destination =
+        case getPlayerPosition eh of
+            Just p  -> p + offset
+            Nothing -> error "The player is dead."
 
 exitDungeon :: ExploringHandler -> GameStatus
 exitDungeon eh =
