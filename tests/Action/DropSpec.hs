c1577b392f7dd7dfe5cee30327f633ab7bd1322b
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-04 09:54:17 +0900

    test: add for the successful item dropping (#301)

--- tests/Action/DropSpec.hs
+++ tests/Action/DropSpec.hs
@@ -5,45 +5,79 @@ module Action.DropSpec
     ) where
 
 import           Action                     (ActionResult (ActionResult, killed, newDungeon, status),
-                                             ActionStatus (Failed))
+                                             ActionStatus (Failed, Ok))
 import           Action.Drop                (dropAction)
-import           Actor                      (inventoryItems, player)
+import           Actor                      (Actor, inventoryItems, player)
 import           Actor.Inventory            (addItem)
 import           Control.Lens               ((&), (.~), (^.))
 import           Control.Monad.Trans.Writer (writer)
 import           Data.Array                 (array)
 import           Data.Maybe                 (fromJust)
-import           Dungeon                    (dungeon, pushActor, pushItem)
+import           Dungeon                    (Dungeon, dungeon, pushActor,
+                                             pushItem)
 import           Dungeon.Identifier         (Identifier (Beaeve))
 import           Dungeon.Map.Cell           (TileIdLayer (TileIdLayer), cellMap)
-import           Dungeon.Map.Tile           (tile)
+import           Dungeon.Map.Tile           (TileCollection, tile)
 import           IndexGenerator             (generator)
-import           Item                       (herb)
+import           Item                       (getName, herb)
 import           Linear.V2                  (V2 (V2))
 import qualified Localization.Texts         as T
 import           Test.Hspec                 (Spec, it, shouldBe)
 
 spec :: Spec
-spec =
-    it "returns a Failed result if there is already an item at the player's foot" $
-    result `shouldBe`
-    writer
-        ( ActionResult
-              { status = Failed
-              , newDungeon = pushActor playerPosition actorWithItem d
-              , killed = []
-              }
-        , [T.itemExists])
+spec = do
+    testDropItemSuccessfully
+    testItemAlreadyExists
+
+testDropItemSuccessfully :: Spec
+testDropItemSuccessfully =
+    it "returns a Ok result if there is no item at the player's foot." $
+    result `shouldBe` expected
   where
-    result = dropAction 0 playerPosition actorWithItem tc d
-    actorWithItem =
-        (\x -> a & inventoryItems .~ x)
-            (fromJust $ addItem herb (a ^. inventoryItems))
-    (a, _) = player ig
-    ig = generator
-    tc = array (0, 0) [(0, tile True True)]
-    d = pushItem playerPosition herb $ dungeon cm Beaeve
+    result =
+        dropAction 0 playerPosition actorWithItem initTileCollection initDungeon
+    expected = writer (expectedResult, expectedLog)
+    expectedResult =
+        ActionResult
+            {status = Ok, newDungeon = dungeonAfterDropping, killed = []}
+    dungeonAfterDropping =
+        pushItem playerPosition herb $
+        pushActor playerPosition actorWithoutItem initDungeon
+    expectedLog = [T.youDropped $ getName herb]
+    (actorWithoutItem, actorWithItem) = playerWithoutAndWithItem
+    playerPosition = V2 1 0
+
+testItemAlreadyExists :: Spec
+testItemAlreadyExists =
+    it "returns a Failed result if there is already an item at the player's foot." $
+    result `shouldBe` expected
+  where
+    result =
+        dropAction 0 playerPosition actorWithItem initTileCollection initDungeon
+    expected = writer (expectedResult, expectedLog)
+    expectedResult =
+        ActionResult
+            {status = Failed, newDungeon = dungeonAfterAction, killed = []}
+    dungeonAfterAction = pushActor playerPosition actorWithItem initDungeon
+    expectedLog = [T.itemExists]
+    (_, actorWithItem) = playerWithoutAndWithItem
+    playerPosition = V2 0 0
+
+initDungeon :: Dungeon
+initDungeon = pushItem (V2 0 0) herb $ dungeon cm Beaeve
+  where
     cm =
         cellMap $
-        array (V2 0 0, V2 0 0) [(playerPosition, TileIdLayer Nothing Nothing)]
-    playerPosition = V2 0 0
+        array (V2 0 0, V2 1 0) [(V2 0 0, emptyTile), (V2 1 0, emptyTile)]
+    emptyTile = TileIdLayer Nothing Nothing
+
+playerWithoutAndWithItem :: (Actor, Actor)
+playerWithoutAndWithItem = (without, with)
+  where
+    without = fst $ player generator
+    with =
+        (\x -> without & inventoryItems .~ x)
+            (fromJust $ addItem herb (without ^. inventoryItems))
+
+initTileCollection :: TileCollection
+initTileCollection = array (0, 0) [(0, tile True True)]
