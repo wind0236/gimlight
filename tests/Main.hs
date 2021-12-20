058736a43dfdbeaf769d6cd67c17cd6b18512aef
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-20 14:09:03 +0900

    test: remove a test of Dungeon (#343)
    The function no longer exists.

--- tests/Main.hs
+++ tests/Main.hs
@@ -9,7 +9,6 @@ import qualified Action.MoveSpec
 import qualified Action.PickUpSpec
 import qualified Action.WaitSpec
 import qualified Dungeon.Map.CellSpec
-import qualified DungeonSpec
 import qualified FovSpec
 import           Test.Hspec           (Spec, describe, hspec)
 
@@ -24,6 +23,5 @@ spec = do
     describe "Action.Move" Action.MoveSpec.spec
     describe "Action.PickUp" Action.PickUpSpec.spec
     describe "Action.Wait" Action.WaitSpec.spec
-    describe "Dungeon" DungeonSpec.spec
     describe "Dungeon.Map.Cell" Dungeon.Map.CellSpec.spec
     describe "Fov" FovSpec.spec
