4b8769d9be4d977c56d58e87d602f97aec34230b
Hiroki Tokunaga <tokusan441@gmail.com>
2022-01-21 14:02:20 +0900

    refactor: extract a function (#393)

--- tests/Dungeon/GenerateSpec.hs
+++ tests/Dungeon/GenerateSpec.hs
@@ -3,9 +3,12 @@ module Dungeon.GenerateSpec
     ) where
 
 import           Control.Lens                (_1, (^.))
-import           Control.Monad.State         (evalState, evalStateT)
+import           Control.Monad.State         (State, StateT, evalState,
+                                              evalStateT)
+import           Coord                       (Coord)
 import           Data.Map                    (empty)
 import           Data.Tree                   (Tree)
+import           Dungeon                     (Dungeon)
 import qualified Dungeon                     as D
 import           Dungeon.Generate            (generateMultipleFloorsDungeon)
 import           Dungeon.Generate.Config     (Config, config, getMapSize)
@@ -13,9 +16,9 @@ import           Dungeon.Identifier          (Identifier (Beaeve))
 import           Dungeon.Map.Cell            (CellMap, widthAndHeight)
 import           Dungeon.Map.Tile            (TileCollection)
 import           Dungeon.Map.Tile.JSONReader (addTileFile)
-import           IndexGenerator              (generator)
+import           IndexGenerator              (IndexGenerator, generator)
 import           Linear.V2                   (V2 (V2))
-import           System.Random               (mkStdGen)
+import           System.Random               (StdGen, mkStdGen)
 import           Test.Hspec                  (Spec, describe, it, runIO,
                                               shouldBe)
 import           Test.Hspec.QuickCheck       (modifyMaxSuccess)
@@ -43,10 +46,14 @@ generateMap :: TileCollection -> Config -> Int -> Tree CellMap
 generateMap tc cfg g = fmap (^. D.cellMap) tree
   where
     tree =
-        evalState
-            (evalStateT (generateMultipleFloorsDungeon tc cfg Beaeve) generator)
-            (mkStdGen g) ^.
-        _1
+        extractDungeonTree (mkStdGen g) $
+        generateMultipleFloorsDungeon tc cfg Beaeve
+
+extractDungeonTree ::
+       StdGen
+    -> StateT IndexGenerator (State StdGen) (Tree Dungeon, Coord)
+    -> Tree Dungeon
+extractDungeonTree g = (^. _1) . flip evalState g . flip evalStateT generator
 
 generateConfig :: Gen Config
 generateConfig = fmap tupleToConfig $ arbitrary `suchThat` validValue
